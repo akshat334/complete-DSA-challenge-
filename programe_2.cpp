ecc30fda805cffaa6dabaf28f537530f35086add
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-08-22 22:04:43 +0530

    Add files via upload

---
+++ programe_2.cpp
@@ -0,0 +1,16 @@
+//wap to check number is even or odd
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout << "enter the number :";
+cin >> a;
+if( a%2 == 0){
+cout <<"number is even";
+}
+else{
+    cout <<"number is odd";
+}
+
+}
