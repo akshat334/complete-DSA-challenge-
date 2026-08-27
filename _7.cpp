b69214cbacfe8d577664b3f17b1440b6dc4696f8
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-08-27 20:12:48 +0530

    Add files via upload

---
+++ _7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enther the number a:";
+    cin>>a;
+    if(a>=90)
+    {cout<<"A";
+    }
+    else if(a>=80)
+    {
+        cout<<"B";
+    }
+    else
+    {
+        cout<<"C";
+    }
+    }
+
