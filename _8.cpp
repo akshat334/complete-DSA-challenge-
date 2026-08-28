7864f179e18982d66aee2f50b2372bd86fc0a429
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-08-28 22:28:50 +0530

    Add files via upload

---
+++ _8.cpp
@@ -0,0 +1,19 @@
+//find the uppercase letter's and lowercase letter's 
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a, A;
+    cout<<"enter the value of a";
+    cin>>a;
+
+    if ( a >= 'A' )
+    {
+        cout<<"uppercase letter ";
+    }
+    else
+    {
+        cout<<"lowercase letter ";
+
+    }
+}
