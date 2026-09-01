50b5e0c5c895d9038bbb9074345ffc15f6d6c2fa
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-09-01 21:47:42 +0530

    Add files via upload

---
+++ class18.cpp
@@ -0,0 +1,17 @@
+// wap to demonstrate array in c++
+#include<iostream>
+using namespace std;
+int main()
+
+    
+    {
+        int arr[5] = {1, 2, 3, 4, 5};
+        cout<<arr[0]<<endl;
+        cout<<arr[1]<<endl;
+        for(int i=0; i<5; i++){
+            cout<<arr[i]<<endl;
+        }
+        return 0;
+    }
+
+    
