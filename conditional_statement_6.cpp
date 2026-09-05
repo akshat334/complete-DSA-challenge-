61814ad4a8bf07d315302be7dcb39736fbf366af
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-09-05 21:53:20 +0530

    Add files via upload

---
+++ conditional_statement_6.cpp
@@ -0,0 +1,19 @@
+  
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a;
+    cout<<"enter the value of a";
+    cin>>a;
+    if(a>=18)
+    {
+        cout<<"can vote ";
+
+    }
+    else
+    {
+        cout<<"cannot vote ";
+    }
+}    
+    
