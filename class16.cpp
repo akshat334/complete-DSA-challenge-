b55ba596382b8ad5d56dfb54d5d6c1a949441fd8
Akshat Dwivedi <akshatdwivedi334@gmail.com>
2026-09-10 22:27:14 +0530

    Add files via upload

---
+++ class16.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fibo(int n){
+    if(n<=1){
+        return n;
+    }else{
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    cout<<"The "<<n<<"th term of fibonacci series is: "<<fibo(n)<<endl;
+    return 0;
+}   
